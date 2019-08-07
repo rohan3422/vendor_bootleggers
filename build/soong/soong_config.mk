AospExtended/platform_vendor_aosp

soong: add sdclang
Change-Id: Ib2b955f76ed15934b11fa2f7b58cf71ea772077a

build/soong/soong_config.mk
@@ -21,6 +21,7 @@ $(call add_json_bool, Uses_metadata_as_fde_key, $(filter true,$(TARGET_USES_META
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device, $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Target_use_sdclang,       $(filter true,$(TARGET_USE_SDCLANG)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END

rohan3422/vendor_bootleggers

build/soong/soong_config.mk
@@ -17,6 +17,7 @@ $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECI
 $(call add_json_bool, Uses_nvidia_enhancements,              $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))
 $(call add_json_bool, Uses_qcom_bsp_legacy,                  $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 $(call add_json_bool, Uses_qti_camera_device,                $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
+$(call add_json_bool, Target_use_usdclang,       $(filter true,$(TARGET_USE_SDCLANG)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END